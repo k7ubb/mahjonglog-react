73bc25e89f24d397c2f0c4e5dd2ad1df3c95fc26
k7ubb <kubota.sou@vega.ocn.ne.jp>
2024-04-20 00:45:49 +0900

    firebaseの設定を変更

--- src/lib/firebase/firestore.ts
+++ src/lib/firebase/firestore.ts
@@ -2,32 +2,31 @@ import { getFirestore, collection, doc, getDoc, getDocs, addDoc, setDoc, updateD
 import { FirebaseApp } from 'lib/firebase/firebase';
 
 const app = FirebaseApp();
-const datastorePrefix = process.env.REACT_APP_FIRESTORE_PREFIX;
 
 export const firestoreGets = async (collectionName: string) => {
-  return await getDocs(collection(getFirestore(app), datastorePrefix + collectionName));
+  return await getDocs(collection(getFirestore(app), collectionName));
 };
 
 export const firestoreGet = async (collectionName: string, document: string) => {
-  return await getDoc(doc(getFirestore(app), datastorePrefix + collectionName, document));
+  return await getDoc(doc(getFirestore(app), collectionName, document));
 };
 
 export const firestoreSet = async (collectionName: string, document: string, data: any) => {
-  return await setDoc(doc(getFirestore(app), datastorePrefix + collectionName, document), data);
+  return await setDoc(doc(getFirestore(app), collectionName, document), data);
 };
 
 export const firestoreAdd = async (collectionName: string, data: any) => {
-  return await addDoc(collection(getFirestore(app), datastorePrefix + collectionName), data);
+  return await addDoc(collection(getFirestore(app), collectionName), data);
 };
 
 export const firestoreUpdate = async (collectionName: string, document: string, data: any) => {
-  return await updateDoc(doc(getFirestore(app), datastorePrefix + collectionName, document), data);
+  return await updateDoc(doc(getFirestore(app), collectionName, document), data);
 };
 
 // ==, !=を使う。 ===, !==は使えない
 export const firestoreGetsQuery = async (collectionName: string, key: string, operator: string, value: any) => {
   const queryRef = await getDocs(query(
-    collection(getFirestore(app), datastorePrefix + collectionName),
+    collection(getFirestore(app), collectionName),
     where(key, operator as WhereFilterOp, value)
   ));
   const result: Array<any> = [];
